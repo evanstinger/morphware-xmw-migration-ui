0e1d57af1db12da8c7df0b1c6c1bfd6e3814e606
Evan Stinger <stefanustrg@gmail.com>
2026-03-10 01:49:51 +0800

    vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     public: {
       walletConnectProjectId: process.env.NUXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '',
       migrationContractAddress: process.env.NUXT_PUBLIC_MIGRATION_CONTRACT_ADDRESS || '',
-      chainId: process.env.NUXT_PUBLIC_CHAIN_ID || '56'
+      chainId: process.env.NUXT_PUBLIC_CHAIN_ID || '1'
     }
   },
 
@@ -23,24 +23,7 @@ export default defineNuxtConfig({
     '/': { prerender: true }
   },
 
-  compatibilityDate: '2025-01-15',
-
-  nitro: {
-    preset: 'cloudflare_module',
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true,
-      wrangler: {
-        name: 'morphware-xmw-migration-ui',
-        compatibility_date: '2026-01-20',
-        vars: {
-          NUXT_PUBLIC_WALLETCONNECT_PROJECT_ID: '67553a5ab5967c7fce9a5cccdb2de398',
-          NUXT_PUBLIC_MIGRATION_CONTRACT_ADDRESS: '0xC1567D4793dD42804ec25884B72eCc7AC339bef9',
-          NUXT_PUBLIC_CHAIN_ID: '1'
-        }
-      }
-    }
-  },
+  compatibilityDate: '2026-02-20',
 
   eslint: {
     config: {
